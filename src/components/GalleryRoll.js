0a9e4c929f077f40a157c1c2e89f436b506dd0e5
Svapnil <svapnila@gmail.com>
2020-03-27 03:32:43 -0400

    added prototype gallery page

---
+++ src/components/GalleryRoll.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery } from 'gatsby'
+import PreviewCompatibleImage from './PreviewCompatibleImage'
+import Img from "gatsby-image"
+
+
+const NonStretchedImage = props => {
+    let normalizedProps = props
+    if (props.fluid && props.fluid.presentationWidth) {
+      normalizedProps = {
+        ...props,
+        style: {
+          ...(props.style || {}),
+          maxWidth: 200,
+          margin: "0 auto", // Used to center the image
+        },
+      }
+    }
+  
+    return <Img {...normalizedProps} />
+  }
+
+class TestimonialRoll extends React.Component {
+  render() {
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
+
+    return (
+      <div className="columns is-multiline">
+        {posts &&
+          posts.map(({ node: post }) => (
+            <div className="is-parent column is-4" key={post.id}>
+              <article
+                className={`blog-list-item tile is-child box notification`}
+              >
+                <div className="gallery-thumbnail">
+                    <NonStretchedImage fixed={post.frontmatter.featuredimage.childImageSharp.fixed} />
+                </div>
+                <header>
+                  <p className="post-meta">
+                    <div
+                      className="title has-text-primary is-size-4"
+                      style={{paddingRight: "1em"}}
+                    >
+                    {post.frontmatter.title}
+                    </div>
+                  </p>
+                </header>
+                <p>
+                  {post.frontmatter.description}
+                </p>
+              </article>
+            </div>
+          ))}
+      </div>
+    )
+  }
+}
+
+TestimonialRoll.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+    query GalleryRemark {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "gallery" } } }
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 400)
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                description
+                featuredimage {
+                    childImageSharp {
+                        fixed(width: 304, height: 200, quality: 90) {
+                          ...GatsbyImageSharpFixed
+                        }
+                      }
+                }
+                location
+              }
+            }
+          }
+        }
+	}
+    `}
+    render={(data, count) => <TestimonialRoll data={data} count={count} />}
+  />
+)
