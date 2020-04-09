666c9e7d50750477e52032c6fc518f487f69b5d4
Svapnil <svapnila@gmail.com>
2020-04-09 18:25:28 -0400

    added testimonial page

--- src/pages/testimonials/index.js
+++ src/pages/testimonials/index.js
@@ -10,7 +10,7 @@ export default class TestimonialIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
+            backgroundImage: `url('/img/testimonial-index.jpg')`,
           }}
         >
           <h1
