e91a9f7f09ef3520c07a2ab85201f4ed2cd8bf2a
Svapnil <svapnila@gmail.com>
2020-04-13 16:03:37 -0400

    added full logopic to about page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
-
+import fullLogoPic from '../img/fullLogoPic.png'
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
@@ -13,6 +13,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
+              <img src={fullLogoPic} style={{width: "50%", display: "block", marginLeft: "auto", marginRight: "auto"}}/>
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
