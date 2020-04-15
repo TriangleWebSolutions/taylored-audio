50546913f6c0c5275a979174c37dfeb0d9379cec
Svapnil <svapnila@gmail.com>
2020-04-14 21:44:32 -0400

    fixed colors for headers

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -16,10 +16,12 @@ export default class GalleryIndexPage extends React.Component {
           <h1
             className="has-text-weight-bold is-size-1"
             style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
+              boxShadow:
+                '#0A543D 0.5rem 0px 0px, #0A543D -0.5rem 0px 0px',
+              backgroundColor: '#0A543D',
               color: 'white',
-              padding: '1rem',
+              lineHeight: '1',
+              padding: '0.25em',
             }}
           >
             Gallery
