60080d497ed07a95216251d11eebad32dd9969ac
Triangle Web Solutions <60792166+TriangleWebSolutions@users.noreply.github.com>
2020-04-30 14:23:33 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,160 +1,34 @@
 import React from 'react'
-import {
-  Link
-} from 'gatsby'
+import { Link } from 'gatsby'
 import facebook from '../img/facebook-icon.png'
 import instagram from '../img/instagram-icon.png'
 import tws from '../img/tws-icon.svg'
 import logo from '../img/logo.svg'
 
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      navBarActiveClass: '',
-    }
-  }
-
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState({
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active ?
-          this.setState({
-            navBarActiveClass: 'is-active',
-          }) :
-          this.setState({
-            navBarActiveClass: '',
-          })
-      }
-    )
-  }
-
-  render() {
-    return ( <
-      nav className = "navbar is-transparent"
-      role = "navigation"
-      aria - label = "main-navigation" >
-      <
-      div className = "container" >
-      <
-      div className = "navbar-brand" >
-      <
-      Link to = "/"
-      className = "navbar-item"
-      title = "Logo" >
-      <
-      img src = {
-        logo
-      }
-      alt = "Taylored Audio"
-      style = {
-        {
-          width: '88px'
-        }
-      }
-      /> <
-      /Link> {
-        /* Hamburger menu */ } <
-      div className = {
-        `navbar-burger burger ${this.state.navBarActiveClass}`
-      }
-      data - target = "navMenu"
-      onClick = {
-        () => this.toggleHamburger()
-      } >
-      <
-      span / >
-      <
-      span / >
-      <
-      span / >
-      <
-      /div> <
-      /div> <
-      div id = "navMenu"
-      className = {
-        `navbar-menu ${this.state.navBarActiveClass}`
-      } >
-      <
-      div className = "navbar-start has-text-centered" >
-      <
-      Link className = "navbar-item"
-      to = "/about" >
-      About <
-      /Link> <
-      Link className = "navbar-item"
-      to = "appointments" >
-      Appointments <
-      /Link> <
-      Link className = "navbar-item"
-      to = "/gallery" >
-      Gallery <
-      /Link> <
-      Link className = "navbar-item"
-      to = "/testimonials" >
-      Testimonials <
-      /Link> <
-      Link className = "navbar-item"
-      to = "/contact" >
-      Contact <
-      /Link> <
-      /div> <
-      div className = "navbar-end has-text-centered" >
-      <
-      a className = "navbar-item"
-      href = "https://www.facebook.com/tailoredaudio"
-      target = "_blank"
-      rel = "noopener noreferrer" >
-      <
-      span className = "icon" >
-      <
-      img src = {
-        facebook
-      }
-      alt = "Triangle Web Solutions" / >
-      <
-      /span> <
-      /a> <
-      a className = "navbar-item"
-      href = "https://www.instagram.com/tayloredaudio/"
-      target = "_blank"
-      rel = "noopener noreferrer" >
-      <
-      span className = "icon" >
-      <
-      img src = {
-        instagram
-      }
-      alt = "Triangle Web Solutions" / >
-      <
-      /span> <
-      /a> <
-      a className = "navbar-item"
-      href = "https://triangleweb.solutions"
-      target = "_blank"
-      rel = "noopener noreferrer" >
-      <
-      span className = "icon" >
-      <
-      img src = {
-        tws
-      }
-      alt = "Triangle Web Solutions" / >
-      <
-      /span> <
-      /a> <
-      /div> <
-      /div> <
-      /div> <
-      /nav>
-    )
-  }
-}
-
-export default Navbar
+@@ -77,6 +79,26 @@ const Navbar = class extends React.Component {
+              </Link>
+            </div>
+            <div className="navbar-end has-text-centered">
+              <a
+                className="navbar-item"
+                href="https://www.facebook.com/tailoredaudio"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="icon">
+                  <img src={facebook} alt="Triangle Web Solutions" />
+                </span>
+              </a>
+              <a
+                className="navbar-item"
+                href="https://www.instagram.com/tayloredaudio/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="icon">
+                  <img src={instagram} alt="Triangle Web Solutions" />
+                </span>
+              </a>
+              <a
+                className="navbar-item"
+                href="https://triangleweb.solutions"
